70664bbf4d16773080da87d8fe30ffec03cf2feb
FawadJavid <fawadktk28@gmail.com>
2021-11-09 13:08:30 +0500

    CHanged acording to my Profile

--- src/Others/GlobalVars.ts
+++ src/Others/GlobalVars.ts
@@ -11,14 +11,14 @@ export class GlobalVars {
         '#64DD17', '#AEEA00', '#FFD600', '#FFAB00', '#FF6D00',
         '#DD2C00'
     ]
-    static myName = 'Fayaz Bin Salam'
-    static myOccupation = 'CEO at Rich IT'
-    static myDetails = 'Hello, I am Fayaz Bin Salam. I love Programming, making application/software for Mobile, Web and Desktop. For me, A Computer or A Mobile phone is nothing but some plastics and metals. But when it understands my language, it feels like a miracle!\nI started programming as a hobby since 2009. Wrote my first few scripts in Batch and VBScript. Later I learned C, C++, Java, Python, JavaScript etc, Worked in so many libraries & frameworks like ReactJS(one of my favorites), Angular2+, NodeJS, worked using so many technologies like Git, Arduino, etc. It does feel amazing to see myself improving. Hope to improve everything around me one day...'
+    static myName = 'Fawad Javid'
+    static myOccupation = 'Penetration Tester / Cyber Security Engineer'
+    static myDetails = 'Hello, I am Fawad Javid. I am a Cyber Security Expert and a Hacker with an Ethical mindset. Currently working as an Pentester at Systems Ltd, and a freelancer, I have helped companies secure themselves as a freelancer. I m having experience in Web Application Security, Mobile Application Security, API Security, Source Code Analysis, Vulnerability Assessment and Penetration Testing and excellent proficiency in some of the best tools like IBM AppScan, Burpsuite, Acunetix, Nessus, Postman and SoapUI.'
     static myLeftAvatarImage = null
     static myRightAvatarImage = null
 
     static routes: Array<string> = [
-        "About",
+        "Whoami",
         "Skills",
         "Projects",
         "Contact",
