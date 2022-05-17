31d673d22670abea0d52c7b12714414097331c5f
Fawad Javid <fawadktk28@gmail.com>
2022-05-17 13:18:08 +0500

    modified globalvars file

--- src/Others/GlobalVars.ts
+++ src/Others/GlobalVars.ts
@@ -140,7 +140,7 @@ export class GlobalVars {
 
     static projects: Array<TitledListItemInterface> = [
         {
-            text: "University Projects",
+            text: "Projects",
             arr: [
                 {
                     title: '...',
@@ -157,25 +157,14 @@ export class GlobalVars {
             text: "Contact me",
             arr: [
                 {
-                    title: 'Skype',
-                    logo: 'https://image.flaticon.com/icons/svg/174/174869.svg',
-                    desc: 'p32929',
-                    link: 'skype:p32929?chat'
+                    title: 'LinkedIn',
+                    logo: 'https://seeklogo.com/images/L/linkedin-in-icon-logo-2E34704F04-seeklogo.com.png',
+                    link: 'https://pk.linkedin.com/in/fawad-javid-b25474180'
                 },
                 {
-                    title: 'WhatsApp',
-                    logo: 'https://image.flaticon.com/icons/svg/733/733585.svg',
-                    link: 'https://api.whatsapp.com/send?phone=8801796306262'
-                },
-                {
-                    title: 'Facebook',
-                    logo: 'https://image.flaticon.com/icons/svg/124/124010.svg',
-                    link: 'https://www.facebook.com/p32929'
-                },
-                {
-                    title: 'Anywhere on the internet',
-                    logo: 'https://image.flaticon.com/icons/png/512/364/364089.png',
-                    link: '@p32929'
+                    title: 'Email',
+                    logo: 'https://seeklogo.com/images/G/google-gmail-logo-620D76A63C-seeklogo.com.png',
+                    link: 'Fawadktk28@gmail.com'
                 }
             ]
         },
@@ -185,70 +174,9 @@ export class GlobalVars {
                 {
                     title: 'GitHub',
                     logo: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
-                    link: 'https://github.com/p32929/'
-                },
-                {
-                    title: 'Wordpress',
-                    logo: 'https://lh3.googleusercontent.com/FKoZImII3S1CKxDwudC2k1EW8NMDi8FyWRk0_AufAQcixOd-kdll5H32L2lKvGIbVWIB',
-                    link: 'https://p32929.wordpress.com/'
-                },
-                {
-                    title: 'YouTube',
-                    logo: 'https://lh3.googleusercontent.com/lMoItBgdPPVDJsNOVtP26EKHePkwBg-PkuY9NOrc-fumRtTFP4XhpUNk_22syN4Datc',
-                    link: 'https://www.youtube.com/channel/UCFucp3uZ96IBYxG8755AniQ'
-                },
-                {
-                    title: 'Google Play',
-                    logo: 'https://www.vectorlogo.zone/logos/google_play/google_play-icon.svg',
-                    link: 'https://play.google.com/store/apps/developer?id=Rich+IT&hl=en'
+                    link: 'https://fawadjavid.github.io/'
                 },
             ]
         },
-        {
-            text: "Apps I rarely use",
-            arr: [
-                {
-                    title: 'LinkedIn',
-                    logo: 'https://image.flaticon.com/icons/svg/174/174857.svg',
-                    link: 'https://www.linkedin.com/in/p32929/'
-                },
-                {
-                    title: 'Reddit',
-                    logo: 'https://image.flaticon.com/icons/svg/174/174866.svg',
-                    link: 'https://www.reddit.com/user/p32929'
-                },
-                {
-                    title: 'Twitter',
-                    logo: 'https://image.flaticon.com/icons/svg/733/733579.svg',
-                    link: 'https://twitter.com/p32929'
-                },
-                {
-                    title: 'Instagram',
-                    logo: 'https://image.flaticon.com/icons/svg/2111/2111463.svg',
-                    link: 'https://www.instagram.com/p32929/'
-                },
-                {
-                    title: 'Fiverr',
-                    logo: 'https://99designs-blog.imgix.net/blog/wp-content/uploads/2018/09/fiverr-2018.png?auto=format&q=60&fit=max&w=200',
-                    link: 'https://www.fiverr.com/users/fayazbinsalam/'
-                },
-                {
-                    title: "Freelancer",
-                    logo: "https://lh3.googleusercontent.com/EXMdy4ai6HHl_Ze8vxC7pdQzfS0HFiZ70F2IdPq_VuYropTkrUFyPPlftbPKEbhISg",
-                    link: "https://www.freelancer.com/u/p32929"
-                }
-            ]
-        },
-        {
-            text: "Thank you for visiting my portfolio",
-            arr: [
-                {
-                    title: "Click on the link button",
-                    desc: 'To create your own portfolio. It will only take 3 minutes',
-                    logo: WebImages.giftIcon,
-                    link: 'https://github.com/p32929/p32929.github.io/'
-                }
-            ]
-        }
     ]
 }
